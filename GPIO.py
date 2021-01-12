b48ef61f3429ad5c2194a7028c98b011a2c1d4c4
Florian Glaser <Floriankaze@gmail.com>
2021-01-12 16:57:54 +0100

    init

---
+++ GPIO.py
@@ -0,0 +1,29 @@
+from MCP3008 import MCP3008
+
+class GPIO_Reader(object):
+    """
+    Klasse womit der Aktuelle Lamdawert am GPIO Einglang ausgelesen werden kann
+    """
+    pass
+
+    def __init__ (self):
+        self.adc = MCP3008()
+
+    def getVoltage(self, channel):
+        value = self.adc.read( channel )
+        voltage = value / 1023.0 * 3.3 
+        return round(voltage, 3)
+
+    def getLamda(self, channel):
+        
+        lamda = self.getVoltage(channel) * 3
+        return round(lamda, 3)
+    
+    def getData(self):
+        data = {
+            "lamda1" : self.getLamda(channel = 1),
+            "lamda2" : self.getLamda(channel = 2),
+            "voltage1" : self.getVoltage(channel = 1),
+            "voltage2" : self.getVoltage(channel = 2)
+        }
+        return data
---
+++ starter.py
@@ -0,0 +1,75 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send, emit
+
+from GPIO import GPIO_Reader
+
+from threading import Thread, Event
+
+import time
+import datetime
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+gpio = GPIO_Reader()
+thread = Thread()
+thread_stop_event = Event()
+
+connectionsCounter = 0
+
+
+def updateData(interval):
+    while not thread_stop_event.isSet():
+        data = gpio.getData()
+        socketio.emit("newValues", data, broadcast=True)
+        print(data)
+        time.sleep(interval)
+
+
+@app.route("/")
+def index():
+    
+    now = datetime.datetime.now()
+    timeString = now.strftime("%m.%Y")
+
+    templateData = {
+      'currentMonthYear' : timeString
+      }
+
+    return render_template('index.html', **templateData)
+
+
+@socketio.on('connected')
+def connected(json, methods=['GET', 'POST']):
+
+    global thread
+    global thread_stop_event
+    global connectionsCounter
+
+
+    print('Client connected')
+    connectionsCounter += 1
+
+    if not thread.isAlive():
+        print("Starting Thread")
+        thread_stop_event.clear()
+        # thread = socketio.start_background_task(updateData, 1)
+
+@socketio.on('disconnect')
+def disconnect():
+
+    global connectionsCounter
+    
+    print('Client disconnected')
+    connectionsCounter -= 1
+
+    if connectionsCounter == 0:
+        # Stop Thread
+        global thread_stop_event
+        thread_stop_event.set()
+
+        print('Stopped thread')
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True, port=8081, host='192.168.0.200')
