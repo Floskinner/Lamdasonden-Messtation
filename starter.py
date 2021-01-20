de10e239fb1240cc1deb01f2c48fb335aeaeda56
Florian Glaser <Floriankaze@gmail.com>
2021-01-20 17:13:51 +0100

    Add InfluxDB

--- starter.py
+++ starter.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, send, emit
 
 from GPIO import GPIO_Reader
-
 from threading import Thread, Event
+from influxdb import InfluxDBClient
 
 import time
 import datetime
@@ -17,6 +17,10 @@ thread = Thread()
 thread_stop_event = Event()
 
 connectionsCounter = 0
+isRecording = False
+
+client = InfluxDBClient(host='127.0.0.1', port=8086, username='python',
+                  password='password', database='lamdawerte')
 
 
 def updateData(interval):
@@ -24,19 +28,39 @@ def updateData(interval):
         data = gpio.getData()
         socketio.emit("newValues", data, broadcast=True)
         print(data)
+
+        if isRecording:
+            json_body = [
+                {
+                    "measurement": "lamdawerte",
+                    "tags": {
+                        "Car": "IEinAutoTag"
+                    },
+                    "fields": {
+                        "Lamda_1": data["lamda1"],
+                        "Voltage_1": data["voltage1"],
+                        "Lamda_2": data["lamda2"],
+                        "Voltage_2": data["voltage2"]
+                    }
+                }
+            ]
+
+            client.write_points(json_body)
+            result = client.query('select Lamda_1 from lamdawerte;')
+            print("Result: {0}".format(result))
+
         time.sleep(interval)
-        
-        
+
 
 @app.route("/")
 def index():
-    
+
     now = datetime.datetime.now()
     timeString = now.strftime("%m.%Y")
 
     templateData = {
-      'currentMonthYear' : timeString
-      }
+        'currentMonthYear': timeString
+    }
 
     return render_template('index.html', **templateData)
 
@@ -48,7 +72,6 @@ def connected(json, methods=['GET', 'POST']):
     global thread_stop_event
     global connectionsCounter
 
-
     print('Client connected')
     connectionsCounter += 1
 
@@ -57,11 +80,12 @@ def connected(json, methods=['GET', 'POST']):
         thread_stop_event.clear()
         thread = socketio.start_background_task(updateData, 1)
 
+
 @socketio.on('disconnect')
 def disconnect():
 
     global connectionsCounter
-    
+
     print('Client disconnected')
     connectionsCounter -= 1
 
@@ -75,13 +99,16 @@ def disconnect():
 
 @socketio.on('recording')
 def recording(json):
-    if json["recording"]:
-        #aufnehmen
-        print("start aufnahme")
-    else:
-        #stoppen
-        print("stoppe Aufnahme")
+    global isRecording
     
+    if json["recording"]:
+        isRecording = True
+        print("start aufnahme", isRecording)
+    else:
+        # stoppen
+        isRecording = False
+        print("stoppe Aufnahme")
+
 
 if __name__ == "__main__":
     socketio.run(app, debug=True, port=8081, host='192.168.0.30')
