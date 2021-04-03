effc8ce504f495e96ba7a0b44e0284a34a8696ef
Floskinner <58706771+Floskinner@users.noreply.github.com>
2021-04-03 14:52:49 +0200

    added new average calculation for the values

--- globale_variablen.py
+++ globale_variablen.py
@@ -2,6 +2,7 @@
 Hier weden grundelgende Einstellungen / Variablen definiert, die im ganzen Programm verwendet werden
 """
 
-MESSINTERVAL = 0.5              # in Sekunden (nicht unter 0.5 gehen)
+MESSURE_INTERVAL = 0.05         # in Sekunden
+UPDATE_INTERVAL = 1             # in Sekunden
 DB_DELETE_AELTER_ALS = 180      # in Tage
 AFR_STOCH = 14.70               # Berechnung für AFR
