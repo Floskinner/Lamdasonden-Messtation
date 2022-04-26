188dd42b80abfe3c8e087a8df1a7ce940252baee
Florian Glaser <Floriankaze@gmail.com>
2022-04-26 20:29:31 +0200

    some linting

--- globale_variablen.py
+++ globale_variablen.py
@@ -3,6 +3,6 @@ Hier weden grundelgende Einstellungen / Variablen definiert, die im ganzen Progr
 """
 
 MESSURE_INTERVAL = 0.01         # in Sekunden
-UPDATE_INTERVAL = 1.5             # in Sekunden
+UPDATE_INTERVAL = 1.5           # in Sekunden
 DB_DELETE_AELTER_ALS = 180      # in Tage
 AFR_STOCH = 14.68               # Berechnung für AFR
