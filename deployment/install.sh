b3dc670dfdfc3a2b2b73dfc2ba892173320f0266
Florian Glaser <58706771+Floskinner@users.noreply.github.com>
2025-12-15 15:34:54 +0100

    feat(setup): adding setup scripts which will help to setup the project on the pi

---
+++ deployment/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -eu
+
+echo "Extract python312.tar.gz"
+sudo rm -rf /opt/python312
+sudo tar -xaf python312.tar.gz -C /opt/
+
+sudo /opt/python312/bin/python3 -m pip install --no-index --find-links file:/home/pi/mama/wheels -e .
+
+echo "Install mama application"
+sudo cp deployment/mama.service /etc/systemd/system/
+sudo chown -R root:root /etc/systemd/system/mama.service
+sudo systemctl daemon-reload
+sudo systemctl enable mama.service
+sudo systemctl start mama.service
+sudo systemctl status mama.service
